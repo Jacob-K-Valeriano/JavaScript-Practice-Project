a875b71cc656a3e31bbaf2d0ad6b322899d5ff2b
Jacob-Valeriano <jacob.k.valeriano@gmail.com>
2020-12-21 21:13:31 -0600

    added app.js fucntions, added buttons on CS page. Trouble with full screen and alignment still

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = () => {
+    const song = document.querySelector("song");
+    const play = document.querySelector(".play");
+    const outline = document.querySelector(".moving-outline circle");
+    const video = document.querySelector(".vid-container video")
+
+    //sound
+    const sounds = document.querySelectorAll(".sound-picker button");
+    //Time Display
+    const timeDisplay = document.querySelector(".time-display");
+    //get length of the outline
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+};
+
+app();
